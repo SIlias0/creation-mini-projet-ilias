2089205c74f004f6d5fe357910954ddab4f12616
SIlias0 <ilias.sebbar@gmail.com>
2023-06-06 16:20:22 +0200

    first commit

---
+++ admin/connexion.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<main>
+    <h2>Authentification</h2>
+    
+    <form method="POST" action="">
+        <label for="ident">Identifiant</label>
+        <input type="text" id="ident" name="ident">
+
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password">
+
+        <input type="submit" value="Se connecter">
+    </form>
+</main>
